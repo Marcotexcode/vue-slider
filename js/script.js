8ac3bbc728d178f08657d9dbcb4597614c366faa
Marco Tessitore <marcotexcode@gmail.com>
2021-06-22 15:12:28 +0200

    style and script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,60 @@
 
 
-const sliderImage = new Vule ({
+const sliderImage = new Vue ({
 
-    el: 'sliderImage',
+    el: '#sliderImage',
 
     data: {
-        
+
+        init: 0,
+
+        imagine: [
+            
+            '../img/1.jpg',
+            '../img/2.jpg',
+            '../img/3.jpg',
+            '../img/4.jpg',
+
+        ]
     },
 
     methods: {
+        
+        nextImg() {
+            
+            // Metodo ternario 
+            (this.init == this.imagine.length - 1) ? this.init = 0 : this.init ++;
+
+            // metodo classico 
+            // this.init ++;
+
+            // if (this.init == this.imagine.length ) {
+                
+            //     this.init = 0;
+
+            // }
+        },
+
+        prevImg() {
+
+            //metodo ternario
+            (this.init == 0) ? this.init = this.imagine.length - 1 : this.init --;
+
+            // metodo classico
+            // this.init --;
+
+            // if(this.init == 0) {
+
+            // this.init = this.imagine.length - 1;
+
+            // }
+
+        }
+
 
     }
 
 
 
 });
+
